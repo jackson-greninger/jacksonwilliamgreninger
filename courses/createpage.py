6ece8a479633f8d5a33d30f69c8c8ae42e6a8002
Todd Schmid <toddwayneschmid@gmail.com>
2025-04-04 17:35:17 -0400

    page generator

---
+++ courses/createpage.py
@@ -0,0 +1,38 @@
+from sys import argv
+from os import listdir, fsencode, fsdecode
+
+def create_page(header, footer, body, filename) :
+    file = open(filename, "w")
+    page_string = header + body + footer
+    file.write(page_string)
+    file.close()
+
+if __name__ == "__main__" :
+    # Collect course name
+    course_folder = argv[1]
+
+    # Collect html header
+    header_file = open(f'{course_folder}/header.html')
+    header = header_file.read()
+    header_file.close()
+    
+    # Collect html footer
+    footer_file = open(f'{course_folder}/footer.html')
+    footer = footer_file.read()
+    footer_file.close()
+
+    # Get pages directory
+    pages_folder = fsdecode(f'{course_folder}/pages/')
+
+    # Create pages and spit them into local directory
+    for page_file in listdir(pages_folder) :
+        pagename = fsdecode(page_file)
+        file = open(f"{course_folder}/pages/{pagename}", "r")
+        page = file.read()
+        file.close()
+
+        out_file = f"{course_folder}/csci341_{pagename}"
+        create_page(header, footer, page, out_file)
+
+
+    
